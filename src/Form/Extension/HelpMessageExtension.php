c896f95ea2c96003ea3c7fac2645aaf7db63ef8d
David Molineus <david.molineus@netzmacht.de>
2019-07-26 10:38:09 +0200

    Translate help message

--- src/Form/Extension/HelpMessageExtension.php
+++ src/Form/Extension/HelpMessageExtension.php
@@ -9,6 +9,7 @@
  * @license    LGPL-3.0-or-later https://github.com/netzmacht/contao-form-bundle/blob/master/LICENSE
  * @filesource
  */
+
 declare(strict_types=1);
 
 namespace Netzmacht\ContaoFormBundle\Form\Extension;
