6cbd18cf534e23e8af189cf786d01be56f5259c3
David Molineus <david.molineus@netzmacht.de>
2020-11-24 10:34:37 +0100

    Code style

--- src/Form/DcaForm/Mapper/AbstractWidgetMapper.php
+++ src/Form/DcaForm/Mapper/AbstractWidgetMapper.php
@@ -6,7 +6,7 @@
  * @package    contao-form-bundle
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @copyright  2017-2020 netzmacht David Molineus. All rights reserved.
- * @license    LGPL-3.0 https://github.com/netzmacht/contao-form-bundle/blob/master/LICENSE
+ * @license    LGPL-3.0-or-later https://github.com/netzmacht/contao-form-bundle/blob/master/LICENSE
  * @filesource
  */
 
--- src/Form/DcaForm/Mapper/TextareaWidgetMapper.php
+++ src/Form/DcaForm/Mapper/TextareaWidgetMapper.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Netzmacht Contao Form Bundle.
+ *
+ * @package    contao-form-bundle
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017-2020 netzmacht David Molineus. All rights reserved.
+ * @license    LGPL-3.0-or-later https://github.com/netzmacht/contao-form-bundle/blob/master/LICENSE
+ * @filesource
+ */
+
 declare(strict_types=1);
 
 namespace Netzmacht\ContaoFormBundle\Form\DcaForm\Mapper;
