c6839d6c12b0ea66376031769d8429371dd24498
Benedict Massolle <bm@presentprogressive.de>
2022-04-25 12:48:28 +0200

    Fix cs

--- src/Form/FieldsetType.php
+++ src/Form/FieldsetType.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Netzmacht\ContaoFormBundle\Form;
 
@@ -8,55 +10,55 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use function is_array;
+use function is_callable;
+
 /**
- * Originally a part of https://github.com/adamquaile/AdamQuaileFieldsetBundle
- * @author Adam Quaile
+ * Originally a part of https://github.com/adamquaile/AdamQuaileFieldsetBundle by Adam Quaile
  */
 class FieldsetType extends AbstractType
 {
-    /**
-     * @param OptionsResolver $resolver
-     */
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver
             ->setDefaults([
                 'legend' => '',
                 'inherit_data' => true,
-                'options' => array(),
-                'fields' => array(),
+                'options' => [],
+                'fields' => [],
                 'label' => false,
             ])
             ->addAllowedTypes('fields', ['array', 'callable']);
     }
 
     /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
+     * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        if (!empty($options['fields'])) {
-            if (is_callable($options['fields'])) {
-                $options['fields']($builder);
-            } elseif (is_array($options['fields'])) {
-                foreach ($options['fields'] as $field) {
-                    $builder->add($field['name'], $field['type'], $field['attr']);
-                }
+        if (empty($options['fields'])) {
+            return;
+        }
+
+        if (is_callable($options['fields'])) {
+            $options['fields']($builder);
+        } elseif (is_array($options['fields'])) {
+            foreach ($options['fields'] as $field) {
+                $builder->add($field['name'], $field['type'], $field['attr']);
             }
         }
     }
 
     /**
-     * @param FormView      $view
-     * @param FormInterface $form
-     * @param array         $options
+     * @param array $options
      */
     public function buildView(FormView $view, FormInterface $form, array $options): void
     {
-        if (false !== $options['legend']) {
-            $view->vars['legend'] = $options['legend'];
+        if ($options['legend'] === false) {
+            return;
         }
+
+        $view->vars['legend'] = $options['legend'];
     }
 
     public function getName(): string
